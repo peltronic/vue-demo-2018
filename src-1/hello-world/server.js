c8c9f669ac968e550576cff7316ce3f7d0c8eeee
Peter Gorgone <peter@peltronic.com>
2018-08-03 18:58:52 -0700

    add node server

---
+++ src-1/hello-world/server.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-param-reassign */
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+
+const PRODUCT_DATA_FILE = path.join(__dirname, 'server-product-data.json');
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  next();
+});
+
+app.get('/products', (req, res) => {
+  fs.readFile(PRODUCT_DATA_FILE, (err, data) => {
+    res.setHeader('Cache-Control', 'no-cache');
+    res.json(JSON.parse(data));
+  });
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+});
