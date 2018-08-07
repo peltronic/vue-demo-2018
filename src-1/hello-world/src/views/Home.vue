faa0861d0c723208c7413c196c828072f8a3ba52
Peter Gorgone <peter@peltronic.com>
2018-08-07 13:04:20 -0700

    global CSS for body tag

--- src-1/hello-world/src/views/Home.vue
+++ src-1/hello-world/src/views/Home.vue
@@ -30,3 +30,27 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+html, body {
+    //height: 100%;
+    background: #000;
+    color: #fff;
+    font-family: Lato,sans-serif;
+}
+</style>
+
+<style lang="scss" scoped>
+#app {
+  /*
+  height: inherit;
+  background: #6e6e6e;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  -webkit-align-items: center;
+  justify-content: center;
+  -webkit-justify-content: center;
+  */
+}
+</style>
