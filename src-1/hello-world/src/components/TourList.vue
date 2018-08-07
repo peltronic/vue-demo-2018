936912db9a0879614b99c45ec87b4eb1ca251567
Peter Gorgone <peter@peltronic.com>
2018-08-06 19:38:08 -0700

    Add link elements for thumbnail and heading in grid elements

--- src-1/hello-world/src/components/TourList.vue
+++ src-1/hello-world/src/components/TourList.vue
@@ -1,22 +1,39 @@
 <template>
     <b-container fluid class="bv-example-row">
+
         <b-row id="artistSection" class="grid">
+            <b-col v-for="tour in tours" :key="tour.id" :id="'panel-'+tour.id" class="grid__item tour-list--item">
+                <div rel="canonical" class="background-image" :style="`background-image: url(${tour.thumbnail})`"></div>
+                <a :href="tour.thumbnail" class="img-wrap">
+                    <img rel="canonical" class="fill portrait" v-bind:src="tour.thumbnail">
+                    <div class="description description--grid">
+                    </div>
+                </a>
+                <a :href="tour.thumbnail">
+                    <div class="item-title">
+                        <h6 class="item-sub">{{tour.title}}</h6>
+                        <h3 class="item-heading">{{tour.description}}</h3>
+                        <div class="button"><i class="material-icons">add</i><span>View Project</span></div>
+                    </div>
+                </a>
+            </b-col>
+        </b-row>
+
+        <b-row class="preview">
             <b-col>
-                <h1>Tour List</h1>
+                <button class="action action--close"><i class="material-icons">close</i><span class="text-hidden">Close</span></button>
+                <div class="description description--preview"></div>
             </b-col>
         </b-row>
-        <b-row>
-            <b-col v-for="tour in tours" :key="tour.id" class="tour-list--item">
-                <img v-bind:src="tour.thumbnail"/>
-                {{ tour.title }}
-            </b-col>
-        </b-row>
+
     </b-container>
 </template>
 
 <script>
 //import gridfx from '../gridfx'
 //Object.defineProperty(Vue.prototype, '$gridfx', { value: gridfx });
+
+// https://forum.vuejs.org/t/data-set-in-created-is-not-available-in-mounted/5555
 
 export default {
     name: 'TourList',
@@ -32,13 +49,15 @@ export default {
     },
     data() {
         return {
+            toursToDisplay: 3,
             t: null,
         }
     },
     created() {
-        this.$store.dispatch('getTours');
+        this.$store.dispatch('getTours',this.toursToDisplay);
     },
     mounted() {
+        var tmp = this.$el;
         this.t = new this.$gridfx(document.querySelector('.grid'), {
                         imgPosition: {
                             x: -0.5,
