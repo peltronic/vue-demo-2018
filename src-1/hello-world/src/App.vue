3dadf5be015285f1e00f9f4051b51ae7f292413a
Peter Gorgone <peter@peltronic.com>
2018-08-07 17:51:30 -0700

    move global CSS to App

--- src-1/hello-world/src/App.vue
+++ src-1/hello-world/src/App.vue
@@ -14,12 +14,20 @@
 </template>
 
 <style lang="scss">
+html, body {
+    //height: 100%;
+    background: #000;
+    color: #fff;
+    font-family: Lato,sans-serif;
+}
 #app {
+    /*
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  */
 }
 #nav {
   padding: 30px;
