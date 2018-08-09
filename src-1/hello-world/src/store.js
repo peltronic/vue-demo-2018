ee154ef1d8db5fd0ca1cb34ee73d84bc929e3ace
Peter Gorgone <peter@peltronic.com>
2018-08-08 18:08:09 -0700

    Vuex store impl for /videos api

--- src-1/hello-world/src/store.js
+++ src-1/hello-world/src/store.js
@@ -7,11 +7,15 @@ Vue.use(Vuex)
 export default new Vuex.Store({
     state: {
         tours: [],
+        videos: [],
         featuredVideos: []
     },
     mutations: {
         UPDATE_TOURS (state, payload) {
             state.tours = payload;
+        },
+        UPDATE_VIDEOS (state, payload) {
+            state.videos = payload;
         }
     
     },
@@ -21,10 +25,17 @@ export default new Vuex.Store({
             axios.get('/api/tours?take='+take).then( (response) => {
                 commit('UPDATE_TOURS', response.data);
             });
+        },
+        getVideos({ commit }, take) {
+            // /api/videos
+            axios.get('/api/videos?take='+take).then( (response) => {
+                commit('UPDATE_VIDEOS', response.data);
+            });
         }
     
     },
     getters: {
-        tours: state => state.tours
+        tours: state => state.tours,
+        videos: state => state.videos
     }
 })
