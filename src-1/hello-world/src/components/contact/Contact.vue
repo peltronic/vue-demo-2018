8737275415fa66200755065389827b361598c9f6
Peter Gorgone <peter@peltronic.com>
2018-08-10 10:40:04 -0700

    add contact route, view component for contact page

---
+++ src-1/hello-world/src/components/contact/Contact.vue
@@ -0,0 +1,46 @@
+<template>
+<b-container fluid id="wrap-contact_contact" class="debug-border template-wrap">
+
+    <b-row class="h-100 tag-background_image">
+        <b-col class="my-auto">
+            <div id="page-heading" class="supercrate">
+                    <h2>
+                        <img src="img/logo.png" alt="GTC Logo" height="30">
+                    </h2>
+            </div>
+            <div class="supercrate">
+                <h1>Digital<br />Marketing</h1>
+                <h2>Unlike anything you could imagine</h2>
+            </div>
+        </b-col>
+    </b-row>
+
+</b-container>
+</template>
+
+<script>
+export default {
+    name: 'Contact',
+    props: {
+        msg: String
+    },
+    computed: {
+    },
+    data () {
+        return {
+            foo: 0
+        }
+    },
+    methods: {
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.tag-background_image {
+    background-image: url(/img/img_mountains01.jpg);
+}
+.template-wrap .supercrate {
+    text-align: center;
+}
+</style>
---
+++ src-1/hello-world/src/views/Contact.vue
@@ -0,0 +1,59 @@
+<template>
+    <b-container fluid id="page-contact" class="contact">
+
+        <b-row>
+            <b-col>
+                <Contact msg="foo"/>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col>
+                <Footer msg="foo"/>
+            </b-col>
+        </b-row>
+
+    </b-container>
+</template>
+
+<script>
+// @ is an alias to /src
+//import PageHeader from '@/components/PageHeader.vue'
+import Contact from '@/components/contact/Contact.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  name: 'home',
+  components: {
+    //PageHeader,
+    Contact,
+    Footer
+  }
+}
+</script>
+
+<style lang="scss">
+#page-contact .template-wrap {
+    height: 100vh;
+}
+#page-contact .tag-background_image {
+    background-size: cover;
+    background-position: center center;
+    background-repeat: no-repeat;
+}
+</style>
+
+<style lang="scss" scoped>
+#app {
+  /*
+  height: inherit;
+  background: #6e6e6e;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  -webkit-align-items: center;
+  justify-content: center;
+  -webkit-justify-content: center;
+  */
+}
+
+</style>
